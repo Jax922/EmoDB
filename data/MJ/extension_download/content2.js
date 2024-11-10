53817bf179ea64ed27ff637ed9cabacc8ac92cc3
pci-2 <pci-2@example.com>
2024-11-10 12:45:38 +0800

    update

---
+++ data/MJ/extension_download/content2.js
@@ -0,0 +1,140 @@
+// content.js
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'scrape') {
+        // 在这里写 scrape 逻辑
+        // 比如抓取页面中的内容
+        const links = document.querySelectorAll('a.block'); // 获取所有 class 为 .block 的 <a> 标签
+        const hrefs = [];
+
+        // 遍历所有找到的 <a> 标签
+        links.forEach(link => {
+        const href = link.getAttribute('href');  // 获取 href 属性
+        if (href) {
+            hrefs.push(href);  // 将 href 加入数组
+        }
+        });
+
+        console.log(hrefs);  // 输出所有 href 属性的数组
+        sendResponse({scrapedData: pageData});
+    }
+  });
+
+  let links = []; 
+// 提取页面中的所有 a 标签的 href
+function extractLinks() {
+    // scrollPage();
+    setTimeout(() => {
+        const anchors = document.querySelectorAll('a.block');
+        anchors.forEach(anchor => {
+            const href = anchor.getAttribute('href');
+            const style = anchor.getAttribute('style');
+            const formattedStyle = style.replace(/&quot;/g, '"');
+
+            // 定义正则表达式匹配 2x 的 URL
+            const regex = /url\("([^"]*?)"\)\s*2x/;
+            const match = formattedStyle.match(regex);
+            let url2x = 'N/A'; 
+            if (match && match[1]) {
+                url2x = match[1];
+                console.log('2x的图片URL:', url2x);
+            } 
+            if (href) {
+                links.push(href);  // 存储链接
+                links.push(url2x);
+            }
+        });
+        console.log(`提取了 ${links.length} 个链接`);
+        clickButton();
+    }, 2000);
+}
+
+// 自动点击按钮
+function clickButton() {
+  const button = document.querySelector('button.min-w-fit');
+  if (button) {
+    button.click();  // 点击按钮
+  }
+}
+
+// 将链接保存到文件中（通过与背景脚本通信）
+function saveLinksToFile() {
+//   chrome.runtime.sendMessage({ action: 'saveLinks', links: links }, function(response) {
+//     console.log('Links saved to file:', response);
+//   });
+if (links.length === 0) {
+    console.log("没有可保存的链接");
+    return;
+  }
+
+  // 创建一个 Blob 对象，将链接保存为文本文件
+  const blob = new Blob([links.join("\n")], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+
+  // 创建一个下载链接并触发下载
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'extracted_links.txt';  // 设置文件名
+  document.body.appendChild(a);
+  a.click();  // 触发下载
+  document.body.removeChild(a);  // 移除临时下载链接
+
+  // 释放 URL 对象
+  URL.revokeObjectURL(url);
+  console.log("链接已保存为文件");
+}
+function scrollPage() {
+    window.scrollBy(0, 800);  // 每次滚动100px
+  }
+
+
+  
+// 自动点击并提取链接的循环函数
+function startClicking() {
+  let clickCount = 0;
+  const maxClicks = 500;  // 点击次数
+
+  const interval = setInterval(() => {
+    if (clickCount >= maxClicks) {
+      clearInterval(interval); // 停止循环
+      console.log('点击结束');
+      setTimeout(() => {
+        saveLinksToFile();
+      }, 5000);
+      return;
+    }
+
+    if((links.length/2) % 1000 === 0) {
+        saveLinksToFile();
+    }
+
+    
+    // scrollPage();
+    extractLinks();
+
+    clickCount++;
+    console.log(`点击第${clickCount}次，提取了 ${links.length/2} 个链接`);
+    // 点击按钮并提取链接
+    
+    // saveLinksToFile(links);  // 保存链接到文件
+  }, 3000);  // 每5秒点击一次
+
+
+
+}
+
+
+
+
+// 在页面加载完成后开始执行
+window.addEventListener('load', () => {
+//    first scroll
+    // scrollPage();
+    // setTimeout(() => {
+    //     extractLinks();
+    // }, 2000);
+    // setTimeout(() => {
+    //     startClicking();
+    // }, 5000);
+
+    startClicking();
+});
--- data/MJ/mj.py
+++ data/MJ/mj.py
@@ -1,57 +1,70 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
+
+
+import json
+import os
+import sys
 import time
+from tqdm import tqdm
 import requests
-from bs4 import BeautifulSoup
-
-# 配置 WebDriver
-driver = webdriver.Chrome()
-
-# 打开网页
-driver.get("https://www.midjourney.com/explore?tab=random")  # 替换成你想爬取图片的网页
-
-# 等待页面加载（视情况设置时间，或者使用显式等待）
-time.sleep(5)
-
-body = driver.find_element(By.TAG_NAME, 'body')
-
-html = driver.page_source
-
-# 使用BeautifulSoup解析HTML
-soup = BeautifulSoup(html, 'html.parser')
-
-# 找到id为pageScroll的div
-page_scroll = soup.find(id='pageScroll')
-
-# 提取所有a标签
-links = page_scroll.find_all('a')
-
-# 提取a标签中的href属性
-image_urls = []
-for link in links:
-    href = link.get('href')
-    image_urls.append(href)
-
-for url in image_urls:
-    print(url)
+from datetime import datetime, timezone
 
 
-# # 定位到图片元素（例如通过标签名、class等）
-# images = driver.find_elements(By.TAG_NAME, 'img')  # 根据实际情况选择合适的定位方式
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import utils as UTILS
+import db as DB
+import log as LOG
 
-# # 下载图片
-# for img in images:
-#     src = img.get_attribute('src')
-#     if src:
-#         try:
-#             img_data = requests.get(src).content
-#             with open("images/" + src.split("/")[-1], 'wb') as file:
-#                 file.write(img_data)
-#         except Exception as e:
-#             print(f"Error downloading {src}: {e}")
+save_path = "/home/pci/dong/AIGC-image/mj/"
 
-# # 关闭浏览器
-driver.quit()
+# read the raw json data from ./data/data_2024-11-10 11:56:26.json
+def read_data():
+    with open(os.path.join(os.path.dirname(__file__), 'data_2024-11-10 11:56:26.json')) as f:
+        data = json.load(f)
+    return data
+
+# download the images from the data
+def fetch_image(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Cache-Control": "max-age=0",
+        "Referer": "https://dalle2.gallery/",
+        "If-Modified-Since": "Sat, 09 Nov 2024 07:26:34 GMT",
+        "Sec-CH-UA": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        "Sec-CH-UA-Arch": '"x86"',
+        "Sec-CH-UA-Bitness": '"64"',
+        "Sec-CH-UA-Full-Version": '"130.0.6723.91"',
+        "Sec-CH-UA-Full-Version-List": '"Chromium";v="130.0.6723.91", "Google Chrome";v="130.0.6723.91", "Not?A_Brand";v="99.0.0.0"',
+        "Sec-CH-UA-Mobile": "?0",
+        "Sec-CH-UA-Platform": "Linux",
+        "Sec-CH-UA-Platform-Version": "6.5.0",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+    }
+
+    # 如果需要使用 Cookie，请将其复制到此处
+    cookies = {
+        "AMP_MKTG_437c42b22c": "JTdCJTIycmVmZXJyZXIlMjIlM0ElMjJodHRwcyUzQSUyRiUyRnd3dy5nb29nbGUuY29tLmhrJTJGJTIyJTJDJTIycmVmZXJyaW5nX2RvbWFpbiUyMiUzQSUyMnd3dy5nb29nbGUuY29tLmhrJTIyJTdE",
+        "_gcl_au": "1.1.1498244188.1730992371",
+        "_ga": "GA1.1.1613374330.1730992371",
+        "AMP_437c42b22c": "JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjI3NmVlN2I0ZC1iODUwLTQyN2YtYmFlYS1mMTIzNTIwNzA1MjMlMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzMxMDYzMDkzNjU1JTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYXN0RXZlbnRUaW1lJTIyJTNBMTczMTA2Mzc1MjU1OSUyQyUyMmxhc3RFdmVudElkJTIyJTNBMTM1JTdE",
+        "cf_clearance": "3ryjjX9SVXqMK5FBlKuei_W_QXx6.wQW.JvKCCntqNc-1731063753-1.2.1.1-YR_lEuXUGehkau9aPRKg7QKZkZHqBCv7FdSdGSpjAGxHFv.UVRkXuDRVexCfnx8YIJVvY9ziK1MLSenxXae8oGes4ciOi_QY.tEC4TVMzFT3spNoPTJI6E4olFy4uxlUaBIOrbn8oqUXVwv8e8ijWRtdYLtTzCYP5t43mSt6uOXo9rgTYC0ctXTmazHX_zV3oe7iNwJBm2CTCYZFuQDVCpqdbM6J3nib7cq7teaFATCPzDX_sSlgLzD_84r77FMFHX9UBKsDoWE57YPnSYgVOvos0d7TPhTrzhVQH3sXakVKH7FpyJewVmae2lliAhjDMqT4nOfgmpdWYwLqWQGNRMfEskRsAY94G1_bz5MbXc9ZZEqqqgdbOZ1PKPUuhwrn",
+        "_ga_Q0DQ5L7K0D": "GS1.1.1731211155.4.0.1731211155.0.0.0",
+        "__cf_bm": "GgykjJnMZjtISduj4rsbKbjZM7lQfVQiuzFkn4aTD5k-1731211717-1.0.1.1-7Cja.ZbDkuaayFlJS7ZrtH8cYkmsIV6oNxItXqNW3ydHhpapeIwOIpj66Iqx.YYShDLqlZMDdIJ4VP6tXXJwsw",
+    }
+    res = requests.get(url, headers=headers, cookies=cookies)
+    if res.status_code == 200:
+        with open(save_path + "image.jpg", 'wb') as f:
+            f.write(res.content)
+        LOG.log_message(f"Image successfully downloaded: {save_path}", level='info')
+    else:
+        print(f"Failed to download image. Status code: {res.status_code}")
+        # print(f"Response: {res.text}")  
+        LOG.log_message(f"Failed to download image. Status code: {res.status_code}", level='error')
+
+fetch_image("https://cdn.midjourney.com/00159e23-f228-40e1-92d7-6be5343fe8d9/0_0_384_N.webp?method=shortest&qst=6&quality=15")
